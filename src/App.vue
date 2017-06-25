85323d4b191c611067ccfd93bad4fb10e5b712de
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-06-25 21:31:27 +0200

    Add Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
-<template>  
-    <app-nav></app-nav>
+<template>
+    <div>
+        <app-nav></app-nav>
+        <app-hero></app-hero>
+    </div>
 </template>
 
 <script>
 import Nav from './components/Nav.vue';
+import Hero from './components/Hero.vue';
 
 export default {
     components: {
-        appNav: Nav
+        appNav: Nav,
+        appHero: Hero
     }
 }
 </script>
 
 <style lang="scss">
 @import '~bulma/bulma.sass';
-
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="section container is-flex">
+        <h1>Search for your location</h1>
+        <br>
+        <div class="field has-addons">
+            <p class="control">
+                <input class="input" type="text" placeholder="Search...">
+            </p>
+            <br>
+            <p class="control">
+                <a class="button is-info">
+                    Search
+                </a>
+            </p>
+        </div>
+    </main>
+</template>
+
+<style lang="scss">
+.section {
+    flex-direction: column;
+    align-items: center;
+}
+</style>
