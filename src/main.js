b063f04bf8a3621ceef9c2185c50a470d82f4815
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-06-27 09:28:58 +0200

    Setup axios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
 import * as firebase from 'firebase';
 
 import { store } from './store/store';
 import { config } from './firebaseConfig';
 
 Vue.prototype.$firebase = firebase.initializeApp(config);
+Vue.prototype.$http = axios.create({
+    baseURL: 'https://nightlife-882b0.firebaseio.com'
+});
 
 new Vue({
     el: '#app',
