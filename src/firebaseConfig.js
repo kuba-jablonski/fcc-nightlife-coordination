4121020d4e34a99713c4cf1c3e08d87708d49780
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-06-27 09:18:30 +0200

    Setup firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+  export const config = {
+    apiKey: "AIzaSyDtoiEaKz6u4bp4mAr3IfjgCTbi5WrR84Y",
+    authDomain: "nightlife-882b0.firebaseapp.com",
+    databaseURL: "https://nightlife-882b0.firebaseio.com",
+    projectId: "nightlife-882b0",
+    storageBucket: "",
+    messagingSenderId: "703691228533"
+  };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import * as firebase from 'firebase';
+
+import { store } from './store/store';
+import { config } from './firebaseConfig';
+
+Vue.prototype.$firebase = firebase.initializeApp(config);
 
 new Vue({
     el: '#app',
+    store,
     render: h => h(App)
 })
