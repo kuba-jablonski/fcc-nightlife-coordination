8056cafe74ddfd49cda8524ae453da112f448e55
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-07-06 16:27:14 +0200

    Fix links

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="section container has-text-centered">
-        <p>a <a href="https://github.com/kuba-jablonski" target="_blank">freeCodeCamp<i class="fa fa-free-code-camp"></i></a> Project</p>
-        <p>build by <a href="https://www.freecodecamp.com" target="_blank"><i class="fa fa-github"></i>napstablook</a></p>
+        <p>a <a href="https://www.freecodecamp.com" target="_blank">freeCodeCamp<i class="fa fa-free-code-camp"></i></a> Project</p>
+        <p>build by <a href="https://github.com/kuba-jablonski" target="_blank"><i class="fa fa-github"></i>napstablook</a></p>
         <p>powered by <a href="https://www.yelp.com" target="_blank"><i class="fa fa-yelp"></i> yelp</a></p>
     </div>
 </template>
