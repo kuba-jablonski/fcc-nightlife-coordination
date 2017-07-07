3cf8c511990c480494627fce4e0e196aa00fd2ff
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-07-07 13:48:59 +0200

    Add animation

--- src/components/search/SearchResults.vue
+++ src/components/search/SearchResults.vue
@@ -1,11 +1,13 @@
 <template>
-    <section id="results" class="section container box">
-        <app-search-result-item 
-        v-for="result in searchResults"
-        :key="result.id"
-        :result="result"
-        ></app-search-result-item>
-    </section>
+    <transition name="slide">
+        <section id="results" class="section container box">
+            <app-search-result-item 
+            v-for="result in searchResults"
+            :key="result.id"
+            :result="result"
+            ></app-search-result-item>
+        </section>
+    </transition>
 </template>
 
 <script>
@@ -29,4 +31,20 @@ export default {
         top: -40px
     }
 }
+
+.slide-enter {
+	animation: slide-in-fwd-center 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+}
+
+@keyframes slide-in-fwd-center {
+  0% {
+    transform: translateZ(-1400px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateZ(0);
+    opacity: 1;
+  }
+}
+
 </style>
